0ca1878177b446e8958eb33e56045bd237869cb0
ASRock <developer14688641gmail.com>
2025-03-21 10:39:25 +0300

    Updates

---
+++ precache-manifest.5293f9e1da247a06c07dfc7cb63443ed.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "97f19dbb3d98f0e052e8",
+    "url": "/Social-network/static/css/main.275ade85.chunk.css"
+  },
+  {
+    "revision": "97f19dbb3d98f0e052e8",
+    "url": "/Social-network/static/js/main.97f19dbb.chunk.js"
+  },
+  {
+    "revision": "6123374f149fbd5324b0",
+    "url": "/Social-network/static/css/1.94fca2eb.chunk.css"
+  },
+  {
+    "revision": "6123374f149fbd5324b0",
+    "url": "/Social-network/static/js/1.6123374f.chunk.js"
+  },
+  {
+    "revision": "4835385d901cf250df59",
+    "url": "/Social-network/static/js/2.4835385d.chunk.js"
+  },
+  {
+    "revision": "9fa9fac480bf3de2b765",
+    "url": "/Social-network/static/js/3.9fa9fac4.chunk.js"
+  },
+  {
+    "revision": "c37ec2b774013c06fe2a",
+    "url": "/Social-network/static/js/runtime~main.c37ec2b7.js"
+  },
+  {
+    "revision": "914173c03e7e99f055d0f7ea808e46ac",
+    "url": "/Social-network/static/media/258-2582267_circled-user-male-skin-type-1-2-icon-male-user-icon.914173c0.png"
+  },
+  {
+    "revision": "d957ab65ef67c1115543fcf24b3d0c58",
+    "url": "/Social-network/static/media/tube-spinner.d957ab65.svg"
+  },
+  {
+    "revision": "2b924033f82958b8f1ccd55659e04090",
+    "url": "/Social-network/index.html"
+  }
+];
